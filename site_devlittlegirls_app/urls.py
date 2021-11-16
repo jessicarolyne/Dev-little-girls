40777bfcc537122da7a0480950db3f330fe196af
marciamelo <msouzademelo@yahoo.com.br>
2021-11-16 19:18:25 -0300

    Alterado textos e imagem inicial

--- site_devlittlegirls_app/urls.py
+++ site_devlittlegirls_app/urls.py
@@ -3,5 +3,4 @@ from site_devlittlegirls_app import views
 
 urlpatterns = patterns('',
                        url(r'^', views.index, name='index'),
-#                       url(r'^', views.more, name='more'),
                        )
