a35958545ebb6f9adfd19c11bc07db2b58de9a92
marciamelo <msouzademelo@yahoo.com.br>
2021-11-17 17:58:25 -0300

    Excluida pasta desnecessaria

--- site_devlittlegirls_app - Copia/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from site_devlittlegirls_app.models import Site
-
-class SiteAdmin(admin.ModelAdmin):
-    exclude = ['data']
-    prepopulated_fields = {'slug': ('titulo',)}
-
-admin.site.register(Site)
--- site_devlittlegirls_app - Copia/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, url
-from site_devlittlegirls_app import views
-
-urlpatterns = patterns('',
-                       url(r'^', views.index, name='index'),
-                       )
